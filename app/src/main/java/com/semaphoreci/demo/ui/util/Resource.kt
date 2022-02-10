c74ff939c456f2e55b79a8d2e0dd71b652f5bd58
Gabriel <gabriel.agrelli@objective.com.br>
2022-02-10 10:46:42 -0300

    Fix Detekt error

--- app/src/main/java/com/semaphoreci/demo/ui/util/Resource.kt
+++ app/src/main/java/com/semaphoreci/demo/ui/util/Resource.kt
@@ -4,7 +4,7 @@ sealed class Resource<T>(
     val data: T? = null,
     val message: String? = null
 ) {
-    class Loading<T>() : Resource<T>()
+    class Loading<T> : Resource<T>()
     class Success<T>(data: T) : Resource<T>(data = data)
     class Error<T>(message: String? = null) : Resource<T>(message = message)
 }
